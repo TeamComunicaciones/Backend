01147ee5df8557897d6df8db73e0a0c7de5601ef
sebastian moncada <sebastian7584@hotmail.com>
2023-09-29 09:06:21 -0500

    prueba precios error

--- sqlControl/prueba.py
+++ sqlControl/prueba.py
@@ -1,28 +1,32 @@
-import pyodbc
-import pandas as pd
+# import pyodbc
+# import pandas as pd
 
-drivers = pyodbc.drivers()
-print(drivers)
+# drivers = pyodbc.drivers()
+# print(drivers)
 
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=team.soluciondigital.com.co;DATABASE=Stok;UID=sa;PWD=Soluciondig2015')
+# conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=team.soluciondigital.com.co;DATABASE=Stok;UID=sa;PWD=Soluciondig2015')
 
-cursor = conn.cursor()
+# cursor = conn.cursor()
 
-query= (
-            "SELECT TOP(1000) P.Nombre, lPre.nombre, ValorBruto "  
-            "FROM dbo.ldpProductosXAsociaciones lProd " 
-            "JOIN dbo.ldpListadePrecios  lPre ON lProd.ListaDePrecios = lPre.Codigo " 
-            "JOIN dbo.Productos  P ON lProd.Producto = P.Codigo " 
-            "JOIN dbo.TiposDeProducto  TP ON P.TipoDeProducto = TP.Codigo " 
-            f"WHERE TP.Nombre = 'Prepagos' and P.Visible = 1 and P.Nombre = 'Isphone 12 128 Gb Prepago';"
-        )
+# query= (
+#             "SELECT TOP(1000) P.Nombre, lPre.nombre, ValorBruto "  
+#             "FROM dbo.ldpProductosXAsociaciones lProd " 
+#             "JOIN dbo.ldpListadePrecios  lPre ON lProd.ListaDePrecios = lPre.Codigo " 
+#             "JOIN dbo.Productos  P ON lProd.Producto = P.Codigo " 
+#             "JOIN dbo.TiposDeProducto  TP ON P.TipoDeProducto = TP.Codigo " 
+#             f"WHERE TP.Nombre = 'Prepagos' and P.Visible = 1 and P.Nombre = 'Isphone 12 128 Gb Prepago';"
+#         )
 
-cursor.execute(query)
+# cursor.execute(query)
 
-rows = cursor.fetchall()
-print(len(rows))
+# rows = cursor.fetchall()
+# print(len(rows))
 
-for row in rows:
-    print(row[0])
+# for row in rows:
+#     print(row[0])
 
-conn.close()
+# conn.close()
+
+numero = 1973000
+numero = '${:,.2f}'.format(numero)
+print(numero)
