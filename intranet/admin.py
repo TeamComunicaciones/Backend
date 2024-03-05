bdb4da5bc631f5efcdd89e75f8b407206282f0f6
sebastian moncada <sebastian7584@hotmail.com>
2024-03-04 22:27:32 -0500

    actualizacion comisiones

--- intranet/admin.py
+++ intranet/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 admin.site.register(models.Permisos_precio)
 admin.site.register(models.Permisos_usuarios_precio)
+admin.site.register(models.Porcentaje_comision)
